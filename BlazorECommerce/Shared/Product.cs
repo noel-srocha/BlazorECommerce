21af0acb6f14969d772a22799575000c2c3e5495
Manoel de Souza Rocha Neto <manoelrocha.n93@gmail.com>
2023-07-25 07:15:38 -0300

    Add Category migration

--- BlazorECommerce/Shared/Product.cs
+++ BlazorECommerce/Shared/Product.cs
@@ -10,4 +10,6 @@ public class Product
   public string ImgUrl { get; set; }
   [Column(TypeName = "decimal(18, 2)")]
   public decimal Price { get; set; }
+  public Category? Category { get; set; }
+  public int CategoryId { get; set; }
 }
